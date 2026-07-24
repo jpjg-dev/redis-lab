026c536b110e0a356faa5f67841f38f31c5c8b51
jipi <198682663+jpjg-dev@users.noreply.github.com>
2026-07-24 11:12:17 +0900

    feat: init setting

---
+++ src/main/java/com/jipi/redis_lab/RedisLabApplication.java
@@ -0,0 +1,13 @@
+package com.jipi.redis_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLabApplication.class, args);
+	}
+
+}
